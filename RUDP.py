772fa4c853a2b017c1e1c0e6b823d29894054933
None <None>
2021-11-25 12:01:43 -0800

    Added NACK logic

--- RUDP.py
+++ RUDP.py
@@ -1,8 +1,10 @@
 import socket, optparse
 import netifaces as ni
 from Packet import Packet 
+import select
 import codecs
 import pickle
+import time
 
 class RUDP():
 
@@ -14,37 +16,45 @@ class RUDP():
         self.segmentSize = int(segmentSize)
         self.createConnection()
         self.sequenceMapping = {}
-        self.packetIndex = 0
+        self.packetIndex = -1
 
     def createConnection(self):
         self.s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        self.s.settimeout(2.0)
+        self.s.settimeout(2.0) #setting timeout fro recv n socket
                 
     def sendData(self, filelocation):
         self.f = open(filelocation, 'rb')
-        data = self.f.read(100)
-        self.sequenceMapping[self.packetIndex] = Packet(self.packetIndex, data)
+        data = self.f.read(self.segmentSize)
         self.packetIndex += 1
-        pickleData = pickle.dumps(self.sequenceMapping[self.packetIndex-1])
+        # self.sequenceMapping[self.packetIndex] = Packet(self.packetIndex, data)
+        # pickleData = pickle.dumps(self.sequenceMapping[self.packetIndex])
+        self.sequenceMapping[self.packetIndex] = pickle.dumps(Packet(self.packetIndex, data))
         while True:
-            self.s.sendto(pickleData, (self.dstIP, self.dstPort) )
-            while( not self.waitACK(self.packetIndex-1)):
-                self.s.sendto(pickleData, (self.dstIP, self.dstPort) )
-            self.waitACK(self.packetIndex-1)
-            data = self.f.read(100)
+            self.s.sendto(self.sequenceMapping[self.packetIndex], (self.dstIP, self.dstPort) )
+            while( not self.waitACK(self.packetIndex)):
+                self.s.sendto(self.sequenceMapping[self.packetIndex], (self.dstIP, self.dstPort) )
+            data = self.f.read(self.segmentSize)
             if not data:
                 self.f.close()
                 self.deleteConnection()
                 break
-            self.sequenceMapping[self.packetIndex] = Packet(self.packetIndex, data)
+            del self.sequenceMapping[self.packetIndex]
             self.packetIndex += 1
-            pickleData = pickle.dumps(self.sequenceMapping[self.packetIndex-1])
+            # self.sequenceMapping[self.packetIndex] = Packet(self.packetIndex, data)
+            # pickleData = pickle.dumps(self.sequenceMapping[self.packetIndex])
+            self.sequenceMapping[self.packetIndex] = pickle.dumps(Packet(self.packetIndex, data))
 
     def waitACK(self, sequenceNo):
         try:
-            data, addr = self.s.recvfrom(512)
+            data, addr = self.s.recvfrom(100)
             print(str(sequenceNo) + ":" + data)
-            return True
+            resp = data.split(":")
+            if(resp[0]=="ACK") and int(resp[1]) == sequenceNo:
+                return True
+            if(resp[0]=="NACK"):
+                sequenceNoNack = resp[1]
+                while( not self.waitACK(sequenceNoNack)):
+                    self.s.sendto(self.sequenceMapping[sequenceNoNack], (self.dstIP, self.dstPort) )
         except Exception as e:
             print(e)
             return False
--- myServer.py
+++ myServer.py
@@ -5,11 +5,15 @@ import pickle
 
 # python myclient.py -i 10.0.0.2 -s 100
 # python myServer.py -i 10.0.0.1
-ReceivedPacket = set()
+# ReceivedPacket = set()
+buffer = {}
+nextSequenceNo = 0
 parser = optparse.OptionParser()
 selfip = ni.ifaddresses(str(ni.interfaces()[-1]))[ni.AF_INET][0]['addr']
 # parser.add_option('-i', dest='dstIP', default='127.0.0.1')
 parser.add_option('-p', dest='port', type='int', default=12345)
+parser.add_option('-s', dest='segmentSize', type='int', default=100)
+parser.add_option('-w', dest='bufferSize', type='int', default=5)
 (options, args) = parser.parse_args()
 
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -17,13 +21,26 @@ s.bind( (selfip, options.port) )
 
 f = open(selfip + '_recv.txt', 'wb+')
 while True:
-    data, addr = s.recvfrom(512)
+    data, addr = s.recvfrom(options.segmentSize + 100)
     packet = pickle.loads(data)
-    s.sendto(str(packet.sequenceNo) + ":ACK", (addr[0], addr[1]) )
-    if packet.sequenceNo not in ReceivedPacket:
-        print("%s " % packet.payload)
-        f.write("%s" % packet.payload)
-        ReceivedPacket.add(packet.sequenceNo)
-    # print(addr)
-    # print(str(packet.sequenceNo) + ":" + addr[0])
-    # f.flush()
+    # s.sendto(str(packet.sequenceNo) + ":ACK", (addr[0], addr[1]) )
+    print(buffer)
+    # for key, value in buffer.items():
+    #     print(key)
+    # print(len(buffer))
+    if(len(buffer) >= options.bufferSize):
+        #drop packets
+        print("Exceeded buffer")
+        if packet.sequenceNo == nextSequenceNo:
+            f.write("%s" % packet.payload)
+            nextSequenceNo += 1
+        else:
+            s.sendto("NACK:" + str(nextSequenceNo), (addr[0], addr[1]) )
+    else:
+        s.sendto("ACK:" + str(packet.sequenceNo), (addr[0], addr[1]) )
+        buffer[packet.sequenceNo] = packet.payload
+        while nextSequenceNo in buffer:
+            f.write("%s" % buffer[nextSequenceNo])
+            del buffer[nextSequenceNo]
+            nextSequenceNo += 1
+        f.flush()
